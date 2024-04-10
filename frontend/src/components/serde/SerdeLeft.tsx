b3252410e688b28329a5824289a3ec795168a601
Mark Fehrenbacher <kendopunk@hotmail.com>
2024-04-10 12:47:10 -0400

    Finished up Serde for now

--- frontend/src/components/serde/SerdeLeft.tsx
+++ frontend/src/components/serde/SerdeLeft.tsx
@@ -1,7 +1,7 @@
 /**
  * src/components/serde/SerdeLeft.tsx
  */
-import { Typography, useTheme } from '@mui/material'
+import { Box, Typography, useTheme } from '@mui/material'
 
 export default function SerdeLeft(): JSX.Element {
   const theme = useTheme()
@@ -11,6 +11,27 @@ export default function SerdeLeft(): JSX.Element {
       <Typography variant="h6" sx={{ color: theme.palette.grey[600] }}>
         Serde
       </Typography>
+      <Box sx={{ mb: 1 }}>
+        <Typography variant="body2">
+          <a href="https://serde.rs/" target="_blank" className="hyperlink">
+            Serde
+          </a>{' '}
+          is a framework for (ser)ializing and (de)serializing Rust data structures efficiently and
+          generically.
+        </Typography>
+      </Box>
+      <Box sx={{ mb: 1 }}>
+        <Typography variant="body2">
+          In the first tab, we are using the json!() macro to construct a serde_json::Value from a
+          JSON literal.
+        </Typography>
+      </Box>
+      <Box sx={{ mb: 1 }}>
+        <Typography variant="body2">
+          In the second tab, we are creating a vector of CustomerOrder structs, generating it
+          dynamically with the fake crate, and serializing with Serde through the Actix server.
+        </Typography>
+      </Box>
     </>
   )
 }
